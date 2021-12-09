8ffc36063532a96f65e0bb33a230c5a74b069e67
SavvaShuliatev <71968903+SavvaShuliatev@users.noreply.github.com>
2021-12-09 17:51:40 +0300

    Initial Commit

---
+++ hseHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hseHelper
+//
+//  Created by Савва Шулятьев on 09.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
